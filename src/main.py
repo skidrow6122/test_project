49d7492b6804d58d45650ef3f9bb0d7e09149322
jeonhyeongjun <skidrow6122@gmail.com>
2025-01-21 00:01:43 +0900

    third commit

--- src/main.py
+++ src/main.py
@@ -6,4 +6,4 @@ import pandas as pd
 print("PyTorch version:", torch.__version__)
 print("NumPy array:", np.array([1, 2, 3]))
 print("Pandas DataFrame:", pd.DataFrame({'A': [1, 2], 'B': [3, 4]}))
-
+# end of code
