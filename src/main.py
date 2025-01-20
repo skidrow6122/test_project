4948cb2b18f88f6a64a808cba245d82eebff9f1e
전형준 <jeonhyeongjun@MacBook-Pro.local>
2025-01-20 23:47:52 +0900

    first commit

---
+++ src/main.py
@@ -0,0 +1,8 @@
+import torch
+import numpy as np
+import pandas as pd
+
+print("PyTorch version:", torch.__version__)
+print("NumPy array:", np.array([1, 2, 3]))
+print("Pandas DataFrame:", pd.DataFrame({'A': [1, 2], 'B': [3, 4]}))
+
